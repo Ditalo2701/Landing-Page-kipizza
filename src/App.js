4436b82c0d0502530826332d882ac883f6c94ec8
Your Ditalo <ditalo2701@gmail.com>
2022-02-02 23:33:07 -0300

    site

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Sidebar from './components/Sidebar';
-import Hero from './components/Hero';
-import { BrowserRouter as Router} from 'react-router-dom';
-import { GlobalStyle } from './Global.Style';
-import { productData, productDataTwo } from './components/Product/data'
-import Products from './components/Product';
-import Feature from './components/Feature';
-
-
-
-
-
 
 function App() {
   return (
-          <Router>
-             <GlobalStyle />
-             <Hero />
-             <Sidebar />
-             <Products heading = ' As melhores pizza de São Luís' data = {productData}/>
-             <Feature />
-             <Products heading = ' ' data = {productDataTwo}/>
-          </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
